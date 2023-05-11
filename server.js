35525c54510bc13701745ba7c5ff2ff54fc3b085
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-05-10 20:27:30 -0500

    setup de servidor express.js

---
+++ server.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var http = require("http");
+var WebSocket = require("ws");
+var app = express();
+//initialize a simple http server
+var server = http.createServer(app);
+//initialize the WebSocket server instance
+var wss = new WebSocket.Server({ server: server });
+wss.on('connection', function (ws) {
+    //connection is up, let's add a simple simple event
+    ws.on('message', function (message) {
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        //log the received message and send it back to the client
+        console.log(JSON.parse(message)["loca"]);
+        ws.send("Hello, you sent -> ".concat(message));
+    });
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+//start our server
+server.listen(process.env.PORT || 8999, function () {
+    console.log("Server started on port ".concat(server.address().port, " :)"));
+});
